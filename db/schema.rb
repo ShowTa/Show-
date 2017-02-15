bd1014c83f4e836bf898fac8057068352ecbf1f3
t-kusakabe <ttmac0704@gmail.com>
2017-02-16 03:50:37 +0900

    modify columns in article

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,10 @@ ActiveRecord::Schema.define(version: 20170214003050) do
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.text     "content",    limit: 65535
+    t.integer  "status",     limit: 1,     default: 0, null: false
     t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
   end
 
