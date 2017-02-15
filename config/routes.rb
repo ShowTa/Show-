c358067441393a56810a5a5a734a488bf636aa6b
t-kusakabe <ttmac0704@gmail.com>
2017-02-16 04:20:54 +0900

    add route draft

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :articles do
+    collection do
+      get :draft_index
+    end
     resources :comments, only: :create
   end
 end
