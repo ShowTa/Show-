9f59ca9d013bb6b9519c9913d8bdcd819508e60f
t-kusakabe <ttmac0704@gmail.com>
2017-02-16 16:09:21 +0900

    modify design in mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles.page(params[:page]).per(5).order('created_at DESC')
+    @articles = @user.articles.page(params[:page]).per(4).order('created_at DESC')
   end
 end
