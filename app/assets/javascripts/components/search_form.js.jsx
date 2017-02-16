6c117d1c97a0c27187095b378c11ffdaf2220ade
fuchi15 <sakuya815@gmail.com>
2017-02-17 05:34:58 +0900

    Search from header

---
+++ app/assets/javascripts/components/search_form.js.jsx
@@ -0,0 +1,20 @@
+var SearchForm = React.createClass({
+  getInitialState: function(){
+    return{
+      searchText: ''
+    }
+  },
+  onSearchText: function(e){
+    this.setState({
+      searchText: e.target.value
+    })
+  },
+  render: function(){
+    return(
+      <form className="searchbox" method="get" action={"/search/" + this.state.searchText}>
+        <input type="text" onChange={this.onSearchText} ref="word" placeholder="記事のタイトルで検索できます" />
+        <button type="submit">検索</button>
+      </form>
+    )
+  }
+})
