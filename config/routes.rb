d7cf42078767d68900cd2752e891c229394a54ac
t-kusakabe <ttmac0704@gmail.com>
2017-02-16 10:44:28 +0900

    add route favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'articles#index'
+
   resources :articles do
     collection do
       get :draft_index
     end
     resources :comments, only: :create
+    resource :favorites, only: [:create, :destroy]
   end
 end
