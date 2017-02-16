a79c63f003ccf91bced54796e6f9413a954f8417
TanakaMana <makkuromyaon@gmail.com>
2017-02-16 09:24:09 +0900

    manas commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @articles = Article.all
+    @articles = Article.includes(:user).all
   end
 
   def show
