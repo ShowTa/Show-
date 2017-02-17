8c0a101bf9515f18ee2c065d35eb18387118eb82
t-kusakabe <ttmac0704@gmail.com>
2017-02-17 10:25:29 +0900

    ひとまずこみっと

--- app/assets/javascripts/components/article_show.js.jsx
+++ app/assets/javascripts/components/article_show.js.jsx
@@ -13,7 +13,9 @@ var ArticleShow = React.createClass({
       <div className="article__show">
         <div className="article__show__header">
           <h2 className="article__show__header__title">{this.state.article.title}</h2>
+          <a href={"/articles/" + this.state.article.id + "/favorites"}>いいね</a>
         </div>
+
         <div className="article__show__content">
           <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
         </div>
