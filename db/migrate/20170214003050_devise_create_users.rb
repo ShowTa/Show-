c38184c1837b48698ed776b6ba3b7e25b4055361
t-kusakabe <ttmac0704@gmail.com>
2017-02-17 11:17:33 +0900

    modify user migration

--- db/migrate/20170214003050_devise_create_users.rb
+++ db/migrate/20170214003050_devise_create_users.rb
@@ -5,6 +5,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.text :profile
+      t.text :icon
 
       ## Recoverable
       t.string   :reset_password_token
