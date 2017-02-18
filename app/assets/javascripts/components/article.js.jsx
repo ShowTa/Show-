845992d9c3e8a4e05ed64bf5f91bd377e212b926
t-kusakabe <ttmac0704@gmail.com>
2017-02-18 11:14:03 +0900

    modify link edit とか deleteとか in article.js.jsx

--- app/assets/javascripts/components/article.js.jsx
+++ app/assets/javascripts/components/article.js.jsx
@@ -4,9 +4,12 @@ var Article = React.createClass({
     return (
       <div className="article col-xs-9">
         <h2><a href={"/articles/" + this.props.article.id}>{this.props.article.title}</a></h2>
-        <h5 className='article__author'>作成者 : </h5>
-        <a className='article__author__name' href={"/users/" + this.props.user.id}>{this.props.user.name}
+        <a href={"/users/" + this.props.user.id}>
+          <h5 className='article__author'>作成者 : </h5>
+          <p className='article__author__name'>{this.props.user.name}</p>
         </a>
+        <a href={'/articles/' + this.props.article.id + '/edit'}>編集</a>
+        <a href={'/articles/' + this.props.article.id}>削除</a>
       </div>
     );
   }
